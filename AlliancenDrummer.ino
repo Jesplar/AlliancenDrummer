b7110a6ab8ab8a73b6161f92f3bd723d4c508d28
Jesper Larsson <thejepz@hotmail.com>
2019-11-07 11:16:35 +0100

    pixels.clear() instead of color(0,0,0)

--- AlliancenDrummer.ino
+++ AlliancenDrummer.ino
@@ -5,7 +5,9 @@
   #include <avr/power.h>
 #endif
 
-#define PIN            2
+#define LEDSTRIPPIN    2
+#define MICPIN         9
+#define SELECTORPIN    12 
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      240
@@ -13,7 +15,7 @@
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strip you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDSTRIPPIN, NEO_GRB + NEO_KHZ800);
 bool colorWheel = false;
 long colorIndex;
 
@@ -24,10 +26,12 @@ uint32_t yellow = pixels.Color(255,255,0);
 uint32_t white = pixels.Color(255,255,255);
 uint32_t pink = pixels.Color(255,0,100);
 uint32_t cyan = pixels.Color(0,255,255);
-uint32_t orange = pixels.Color(230,80,0);
-uint32_t currentColor = pixels.Color(0,0,0); 
+uint32_t orange = pixels.Color(230,40,0); 
+uint32_t currentColor = pixels.Color(0,0,0);
+uint32_t balettColor = pixels.Color(230,0,0);
+uint32_t allianceColor = pixels.Color(128,20,128); 
 
-uint32_t colors[] = {red, green, blue, yellow, white, pink, cyan, orange};
+uint32_t colors[] = {red, green, green, blue, yellow, pink, cyan, orange, balettColor, allianceColor, allianceColor};
 int numColors = sizeof(colors) / sizeof(colors[0]);
 
 
@@ -44,52 +48,58 @@ void setup() {
   pinMode(12, INPUT); // Input for color switch
   pinMode(9, INPUT); // Mic input
   digitalWrite(13,HIGH);
-  colorWheel = digitalRead(12); // Colorwheel can't be changed during runtime
+  colorWheel = digitalRead(SELECTORPIN); // Colorwheel can't be changed during runtime
   
   
 }
 
 void loop(){
+ pixels.clear(); 
  if (colorWheel){ // Color wheel mode, when started with colorwheel switch turned on.
-   if (digitalRead(9)){
+   if (digitalRead(MICPIN)){
      colorIndex = random(numColors); 
-     currentColor = colors[colorIndex];
+     currentColor = colors[colorIndex]; // Try this before trying a more sophisticated solution
      for(int i=0;i<NUMPIXELS;i++){
        pixels.setPixelColor(i,currentColor);
      }
      pixels.show();
-     delay(100); // Replace with delayval
+     delay(120); // Replace with delayval
+     /*
      for(int i=0;i<NUMPIXELS;i++){
        pixels.setPixelColor(i,pixels.Color(0,0,0));
      }
+     */
+     pixels.clear();
      pixels.show();
+     delay(10);
+     
    }
-
-   
-   
  }else{ // Standard mode, when started with colorwheel switch turned off.
-    if (digitalRead(9)){
+    if (digitalRead(MICPIN)){
       
-      if (digitalRead(12)){
+      if (digitalRead(SELECTORPIN)){
         for(int i=0;i<NUMPIXELS;i++){
-        pixels.setPixelColor(i,pixels.Color(230,0,0));
+        pixels.setPixelColor(i,balettColor);
         }
         pixels.show();
         delay(90); // Replace with delayval
-        for(int i=0;i<NUMPIXELS;i++){
-          pixels.setPixelColor(i,pixels.Color(0,0,0));
-        }
+        //for(int i=0;i<NUMPIXELS;i++){
+        //  pixels.setPixelColor(i,pixels.Color(0,0,0));
+        //}
+        pixels.clear();
         pixels.show();
       }else{
         for(int i=0;i<NUMPIXELS;i++){
 //        pixels.setPixelColor(i,pixels.Color(190,50,140));
-          pixels.setPixelColor(i,pixels.Color(128,20,128)); 
+          //pixels.setPixelColor(i,allianceColor); 
+          pixels.setPixelColor(i,orange); 
         }
         pixels.show();
         delay(90); // Replace with delayval
-        for(int i=0;i<NUMPIXELS;i++){
-          pixels.setPixelColor(i,pixels.Color(0,0,0));
-        }
+        //for(int i=0;i<NUMPIXELS;i++){
+        //  pixels.setPixelColor(i,pixels.Color(0,0,0));
+        //}
+        pixels.clear();
         pixels.show();
       }
   
